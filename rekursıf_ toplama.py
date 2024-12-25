be84679cf5195d79dfd6d296666b0b403d3211c4
idriskagan <148585694+idriskagan@users.noreply.github.com>
2024-12-25 16:42:04 +0300

    Add files via upload

--- rekursıf_ toplama.py
+++ rekursıf_ toplama.py
@@ -1,9 +1,17 @@
-def ayırma ( sayı, index):
-    if index == len(sayı):
+#Girilen sayının altındaki 3 veya 5'in tüm katlarının toplamını döndürecek şekilde program yazmanız isteniyor.
+#Ek olarak, sayı negatifse, 0 döndürün.
+#Not: Sayı hem 3'ün hem de 5'in katıysa, yalnızca bir kez sayın.
+#Örnek: 10'un altındaki 3 veya 5'in katları olan tüm doğal sayıları listelersek, 3, 5, 6 ve 9'u elde ederiz. Bu katların toplamı 23'tür.
+
+def katlar(sayi):
+    if sayi < 0:
         return 0
-    if sayı[index] <0:
-        return sayı[index] + ayırma(sayı,index+1)
-    else:
-        return ayırma(sayı,index+1)
-dizi = [1,-2,3,-1,4,0]
-print(ayırma(dizi,0))
+    toplam = 0
+    for i in range(sayi):
+        if i % 3 == 0 or i % 5 == 0:  # 3 veya 5'in katıysa
+            toplam += i
+
+    return toplam
+
+sayi = 10
+print(katlar(sayi))
