3c6c83e9394048358f451b7046f0e7ffb6f1f11e
idriskagan <148585694+idriskagan@users.noreply.github.com>
2024-12-28 14:50:43 +0300

    Update Maskeleme.py

--- Maskeleme.py
+++ Maskeleme.py
@@ -1,10 +1,14 @@
+#Genellikle bir şey satın aldığınızda, kredi kartı numaranızın, telefon numaranızın veya en gizli sorunuza verdiğiniz cevabın hala doğru olup olmadığı sorulur.
+# Ancak, biri omzunuzun üzerinden bakabileceği için, bunu ekranınızda göstermek istemezsiniz. Bunun yerine, bunu maskeleriz.
+#Göreviniz, son dört karakter hariç hepsini '#' olarak değiştiren bir fonksiyon yazmaktır.
+#2763547124245#########4245
 def maskeleme(numara):
+
     # Son dört karakteri al
- son4=numara[-4:]
+    son4=numara[-4:]
     
- maske_numara= '#' * (len(numara) - 4) + son4
- return maske_numara
+    maske_numara= '#' * (len(numara) - 4) + son4
+    return maske_numara
 
 numara= "2763547124245"
-Numara = maskeleme(numara)
-print(numara)
+print(maskeleme(numara))
